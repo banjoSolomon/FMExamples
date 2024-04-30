8613065d67c9aaa802bcc209c541e618451ef4da
banjoSolomon <ayomidebanjo68@gmail.com>
2024-04-30 14:17:55 +0100

    message

--- src/Example10.java
+++ src/Example10.java
@@ -4,18 +4,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-//public class Example10 {
-//    public static void main(String[] args)  {
-//        String location = "C:\\Users\\DELL\\Desktop\\Eexesise";
-//        Path path = Paths.get(location);
-//        try{
-//        BufferedWriter writer = Files.newBufferedWriter(path);
-//        writer.write("happy Birthday, Solomon");
-//        writer.flush();
-//        writer.close();
-//
-//    }catch (IOException e) {
-//            System.err.println(e.getMessage());
-//        }
-//    }
-//}
+public class Example10 {
+    public static void main(String[] args)  {
+        String location = "C:\\Users\\DELL\\Desktop\\Eexesise";
+        Path path = Paths.get(location);
+        try{
+        BufferedWriter writer = Files.newBufferedWriter(path);
+        writer.write("happy Birthday, Solomon");
+        writer.flush();
+        writer.close();
+
+    }catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
