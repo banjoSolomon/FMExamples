96e72550c840b5f033f44e002cb619f446cb749c
banjoSolomon <ayomidebanjo68@gmail.com>
2024-05-10 11:50:50 +0100

    message

--- src/Collections.java
+++ src/Collections.java
@@ -1,13 +1,23 @@
 import jason.Person;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 public class Collections {
     public static void main(String[] args) {
-        boolean b = true;
-        Collection<Boolean> booleans = new ArrayList<>();
-        booleans.add(b);
+//        boolean b = true;
+//        Collection<Boolean> booleans = new ArrayList<>();
+//        booleans.add(b);
+
+        Collection<String> words = new TreeSet<>();
+        words.add("hello");
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(1);
+        Set<List<Integer>> set = new HashSet<>();
+        set.add(numbers);
+        System.out.println(set);
 
     }
 
