86a853e21026e08cbd8568997114a77101de6b89
banjoSolomon <ayomidebanjo68@gmail.com>
2024-05-17 16:10:43 +0100

    updated

--- src/jason/BinaryOperatorSample.java
+++ src/jason/BinaryOperatorSample.java
@@ -4,6 +4,7 @@ import java.util.function.BinaryOperator;
 
 public class BinaryOperatorSample {
     public static void main(String[] args) {
-        BinaryOperator<String> binaryOperator = () ->
+        BinaryOperator<Integer> binaryOperator = (number, num) -> number * num;
+       System.out.println(binaryOperator.apply(5,10));
     }
 }
